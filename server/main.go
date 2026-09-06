3280140de71f4cc215aac37f465ebfbb49eb6aaf
map588 <map588@pitt.edu>
2026-09-06 14:31:05 -0400

    chore: bump version to 0.6.0

--- server/main.go
+++ server/main.go
@@ -43,7 +43,7 @@ import (
 const (
 	defaultAddr       = "127.0.0.1:8765" // used only in --addr / stop / status
 	healthServiceName = "memstate"
-	healthVersion     = "0.5.2"
+	healthVersion     = "0.6.0"
 	readyBanner       = "MEMSTATE_READY addr="
 )
 
